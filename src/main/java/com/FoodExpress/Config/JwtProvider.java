dc2b0a12618ec5c8b307cf1e15da8e27ecc24961
SupuniAbeysinghe <supuniab1@gmail.com>
2024-04-29 10:11:09 +0530

    jwt provider error corrected

--- src/main/java/com/FoodExpress/Config/JwtProvider.java
+++ src/main/java/com/FoodExpress/Config/JwtProvider.java
@@ -3,8 +3,10 @@ package com.FoodExpress.Config;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
