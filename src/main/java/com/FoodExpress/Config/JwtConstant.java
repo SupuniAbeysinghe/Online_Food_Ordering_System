da2355f353f428044d9cba9b5f36c0b3648d0af5
SupuniAbeysinghe <supuniab1@gmail.com>
2024-04-29 10:24:22 +0530

    new secret key

--- src/main/java/com/FoodExpress/Config/JwtConstant.java
+++ src/main/java/com/FoodExpress/Config/JwtConstant.java
@@ -1,7 +1,7 @@
 package com.FoodExpress.Config;
 
 public class JwtConstant {
-    public static final String SECRET_KEY = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaahhfjruh368gjfbfbffenjejnfnfnr78373873fnbg3y774";
+    public static final String SECRET_KEY = "aaaaaaaaaaaaaaaaaaaaabbbbaaaaaaaaaaaaahhfjruh368gjfbfbffenjejnfnfnr78373873fnbg3y774";
     public static final String JWT_HEADER = "Authorization";
 
 }
