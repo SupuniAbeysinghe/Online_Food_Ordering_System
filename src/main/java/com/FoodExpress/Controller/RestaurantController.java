c24165a762c016fb4376749382089ca2b9d9d4b0
SupuniAbeysinghe <supuniab1@gmail.com>
2024-05-04 23:16:49 +0530

    error corrected

--- src/main/java/com/FoodExpress/Controller/RestaurantController.java
+++ src/main/java/com/FoodExpress/Controller/RestaurantController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/restaurant")
+@RequestMapping("/api/restaurants")
 public class RestaurantController {
 
     @Autowired
