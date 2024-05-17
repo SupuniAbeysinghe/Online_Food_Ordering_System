119205812ede2b3d4ef3517effe1fda67e1cf82e
SupuniAbeysinghe <supuniab1@gmail.com>
2024-05-17 23:14:59 +0530

    ingredient API completed

--- src/main/java/com/FoodExpress/Controller/IngredientController.java
+++ src/main/java/com/FoodExpress/Controller/IngredientController.java
@@ -10,6 +10,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/admin/ingredients")
 public class IngredientController {
@@ -40,6 +42,22 @@ public class IngredientController {
             ) throws Exception {
         IngredientsItem item = ingredientsService.updateStock(id);
         return new ResponseEntity<>(item, HttpStatus.OK);
+    }
+    @GetMapping("/restaurant/{id}")
+    public ResponseEntity<List<IngredientsItem>> getRestaurantIngredient(
+            @PathVariable Long id
+
+    ) throws Exception {
+        List<IngredientsItem> items = ingredientsService.findRestaurantsIngredients(id);
+        return new ResponseEntity<>(items, HttpStatus.OK);
+    }
+    @GetMapping("/restaurant/{id}/category")
+    public ResponseEntity<List<IngredientCategory>> getRestaurantIngredientCategory(
+            @PathVariable Long id
+
+    ) throws Exception {
+        List<IngredientCategory> items = ingredientsService.findIngredientCategoryByRestaurantId(id);
+        return new ResponseEntity<>(items, HttpStatus.OK);
     }
 
 }
